0f76cd330f8923e643e8e648f73ad13a8afadf57
Jooeon Kim <joo2theeon@gmail.com>
2022-11-20 22:39:02 +0900

    formatting hanoi

--- programmers/level2__hanoi_tower.py
+++ programmers/level2__hanoi_tower.py
@@ -1,15 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12946
 # 하노이의 탑, Level 2
 
-def hanoi(n,start=1,target=3):  #default value
+def hanoi(n, start=1, target=3):  #default value
     onHand = 6 - start - target #able to change the positions
     if n == 1:
-        trace = [[start,target]]
+        trace = [[start, target]]
         return trace
     else:
-        trace1 = hanoi(n-1,start,onHand)  #trace of the top series
-        trace2 = [[start,target]]         #trace of the bottom
-        trace3 = hanoi(n-1,onHand,target) #trace of the top series
+        trace1 = hanoi(n-1, start, onHand)  #trace of the top series
+        trace2 = [[start, target]]         #trace of the bottom
+        trace3 = hanoi(n-1, onHand, target) #trace of the top series
         trace = trace1 + trace2 + trace3
         return trace
 
