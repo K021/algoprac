e99b454a791197da843b9dab8556d80dcbb2acdb
Jooeon Kim <joo2theeon@gmail.com>
2022-11-24 21:59:50 +0900

    add leetcode: medium__two_sum_2.py

--- leetcode/medium__two_sum_2.py
+++ leetcode/medium__two_sum_2.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/two-sum/submissions/
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 from typing import List
 
@@ -7,21 +7,21 @@ from modules import TestManager, TestCase
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         l, r = 0, len(nums) - 1
-        nums = [(val, idx) for idx, val in enumerate(nums)]
-        nums = sorted(nums)
         while l < r:
-            s = nums[l][0] + nums[r][0]
+            s = nums[l] + nums[r]
             if s == target:
-                return [nums[l][1], nums[r][1]]
+                return [l+1, r+1]
             if s > target:
                 r -= 1
             else:
                 l += 1
         raise Exception("No Answer")
 
+
 test_cases = [
-    TestCase(input=([2, 7, 11, 15], 9), output=[0, 1]),
-    TestCase(input=([3, 2, 4], 6), output=[1, 2]),
+    TestCase(input=([2, 7, 11, 15], 9), output=[1, 2]),
+    TestCase(input=([2, 3, 4], 6), output=[1, 3]),
+    TestCase(input=([-1, 0], -1), output=[1, 2]),
 ]
 
 if __name__ == "__main__":
