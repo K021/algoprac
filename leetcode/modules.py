10f2c2d6066fcd1fcf1a4a442cf4eb6c29bb51c8
Jooeon Kim <joo2theeon@gmail.com>
2022-11-24 21:51:30 +0900

    refactoring

--- leetcode/modules.py
+++ leetcode/modules.py
@@ -15,15 +15,15 @@ class TestCase(BaseModel):
 
 
 class TestManager:
-    def __init__(self, inputs_and_answers: Iterable[TestCase]):
-        self.inputs_and_outputs = inputs_and_answers
+    def __init__(self, test_cases: Iterable[TestCase]):
+        self.test_cases = test_cases
 
     def run(self, solution):
-        for case_number, test_case in enumerate(self.inputs_and_outputs):
+        for case_number, test_case in enumerate(self.test_cases):
             output = solution(*test_case.input)
             if output != test_case.output:
                 raise TestFailed(
                     f"[TestCase #{case_number + 1}] "
                     f"Expected {test_case.output}, got {output}"
                 )
-        print(f"All tests passed (Total TestCases: {len(self.inputs_and_outputs)})")
+        print(f"All tests passed (Total TestCases: {len(self.test_cases)})")
