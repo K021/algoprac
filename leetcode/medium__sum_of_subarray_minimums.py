389dc2de35d13e32fbed56f66d21cdbd2ab81579
Jooeon Kim <joo2theeon@gmail.com>
2022-11-27 17:49:46 +0900

    update new solution on leetcode/medium__sum_of_subarray_minimums.py

--- leetcode/medium__sum_of_subarray_minimums.py
+++ leetcode/medium__sum_of_subarray_minimums.py
@@ -4,7 +4,9 @@ from typing import List
 
 from modules import TestManager, TestCase
 
-class Solution:
+
+# Time Limit Exceeded
+class Solution1:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         n = len(arr)
         sumi = 0
@@ -21,6 +23,69 @@ class Solution:
             print(f"{i=}, {v=}, {count=}")
             sumi += v * count
         return sumi % (10**9 + 7)
+
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        stack = []
+        res = [0] * len(arr)
+        for i, a in enumerate(arr):
+            while stack and stack[-1][0] > a:
+                stack.pop()
+            if stack:
+                res[i] = (i - stack[-1][1]) * a + res[stack[-1][1]]
+            else:
+                res[i] = (i + 1) * a
+            stack.append((a, i))
+        return sum(res) % (10 ** 9 + 7)
+
+
+class SolutionOther1:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 10 ** 9 + 7
+
+        # monotonic increasing stack
+        stack = []
+
+        # make a dp array of the same size as the input array
+        dp = [0] * len(arr)
+
+        # populate monotonically increasing stack
+        for i in range(len(arr)):
+            # before pushing an element, make sure all
+            # larger and equal elements in the stack are
+            # removed
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+
+            # calculate the sum of minimums of all subarrays
+            # ending at index i
+            if stack:
+                previousSmaller = stack[-1]
+                dp[i] = dp[previousSmaller] + (i - previousSmaller) * arr[i]
+            else:
+                dp[i] = (i + 1) * arr[i]
+            stack.append(i)
+
+        # add all the elements of dp to get the answer
+        return sum(dp) % MOD
+
+
+class SolutionOther2:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        #Time: O(n)
+        #Space: O(n)
+        modifiedArr = [0] + arr
+        result = [0]*len(modifiedArr)
+        stack = [0]
+        for i in range(len(modifiedArr)):
+            while modifiedArr[stack[-1]] > modifiedArr[i]:
+                stack.pop() 
+            previousIndex = stack[-1] #Guaranteed to be 0 if we end up emptying the stack as we have 0 always there
+            result[i] = result[previousIndex] + (i- previousIndex)*modifiedArr[i]
+            stack.append(i)
+        return sum(result) % (10**9+7)
+
 
 test_cases = [
     TestCase(input=([3, 1, 2, 4],), output=17),
